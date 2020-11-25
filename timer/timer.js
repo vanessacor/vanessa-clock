412cc8c9d139dd115331d329c00d5cb3229dec36
vanessa <vanessapcbarros@gmail.com>
2020-11-25 10:33:23 +0100

    create timer

--- timer/timer.js
+++ timer/timer.js
@@ -6,7 +6,13 @@ const timeDiff = require("../Utils/getTimeDifference");
 let interval;
 
 function startTimer(minutes) {
-    
+  let startTime = new Date(Date.now());
+  let timeTofinish = startTime.setMinutes(startTime.getMinutes() + minutes);
+  interval = setInterval(() => {
+    let countDownTime = timeDiff.getTimeDifference(timeTofinish);
+    countDownTime = format.formatTime(countDownTime);
+    console.log(countDownTime);
+  }, 1000);
 }
 
 startTimer(5);
