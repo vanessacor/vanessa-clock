62b604d59ac295b9b7e8b04748ef87c8fb23a527
vanessa <vanessapcbarros@gmail.com>
2020-11-25 10:01:25 +0100

    create countdown

--- Countdown/countdown.js
+++ Countdown/countdown.js
@@ -1,7 +1,18 @@
 "use strict";
 
+const format = require("../Utils/formatTime");
+const timeDifference = require("../Utils/getTimeDifference");
+
+let interval;
 
 function startCountDown(date) {
-  
+  let futureDate = new Date(date);
+  interval = setInterval(() => {
+    let countDownTime = timeDifference.getTimeDifference(futureDate);
+
+    countDownTime = format.formatTime(countDownTime);
+    console.log(countDownTime);
+  }, 1000);
 }
 
+startCountDown("Fri, 25 Nov 2027 03:04:05");
